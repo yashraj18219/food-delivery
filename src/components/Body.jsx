c090f811eb1fface68fdddd03873f600b91b5bfc
yashraj18219 <yashraj18219@gmail.com>
2025-12-22 22:32:59 +0530

    filter top rated restaurants

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuCard from "./MenuCard";
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData";
 import { menuObj } from "../utils/mockData";
 const Body = () => {
+
+  const [allRestaurants] = useState(resList)
+  let [listRestro,setListRestro] = useState(resList);
+  
   return (
     <div className="body">
       <div className="menu-box">
-          <h2>What's on your mind?</h2>
+        <h2>What's on your mind?</h2>
         <div className="menu-container">
           {menuObj.map((menu) => {
-            return <MenuCard key = {menu.id} menuData={menu} />;
+            return <MenuCard key={menu.id} menuData={menu} />;
           })}
         </div>
       </div>
 
       <div className="search-bar">
         <input type="text" />
-        <button>Submit</button>
+        <button
+          className="filter-restro"
+          onClick={() => {
+            const filteredList = allRestaurants.filter((res) => {
+              return res.info.avgRating > 4.4;
+            });
+            setListRestro(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => {
+        {listRestro.map((restaurant) => {
           return (
             <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           );
--- src/utils/mockData.jsx
+++ src/utils/mockData.jsx
@@ -1,4 +1,4 @@
-export const resList = [
+export let resList = [
   {
     info: {
       id: "766502",
